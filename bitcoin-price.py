4a2281565782539db05b0c4d87e2f24b2ac93e87
Roman <cypherpanda@pm.me>
2019-10-07 13:00:48 +0200

    edited script

--- bitcoin-price.py
+++ bitcoin-price.py
@@ -1,8 +1,10 @@
 import requests
+
 bitcoin_api_url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
 response = requests.get(bitcoin_api_url)
 response_json = response.json()
-type(response_json) # The API returns a list
+type(response_json)  # The API returns a list
 
-response_json[0]
-print(response_json[0])
+print(response_json[0]['name'])
+print(("Price: ") + (response_json[0]['price_usd']))
+print(("Market cap: ") + (response_json[0]['market_cap_usd']))
